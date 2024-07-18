e10bb0f29b10da17ffbb80a8da6092c2e02a4347
J Neo <jo.neoyj@gmail.com>
2024-07-18 13:07:58 +0800

    added part 2, changed reference creation to be inside function instead

--- OCaml_Intro_to_CS/part_12.ml
+++ OCaml_Intro_to_CS/part_12.ml
@@ -1,30 +1,4 @@
-(* Suppose we have a slow function f : 'a -> 'b, which we may wish to run multiple
-times. If our function is purely functional – that is, if it will always produce the same output
-when given the same input – then we can speed up subsequent calls by remembering the result.
-Please implement cache : ('a -> 'b) -> 'a -> 'b that, when given an input
-function f creates a new function that checks to see if f has been called before on a given input
-and then if so returns the result immediately (technically, in O(log m) time, where m is the
-number of distinct inputs that have been called so far). Hint: reference + dictionary.
-For example, suppose we have the (very inefficient) Fibonacci function from assignment 1:
-let rec fib n = if n = 0 || n = 1 then 1
-else fib (n-1) + fib (n-2);;
-Then if we run cache on fib:
-let cache_fib = cache fib;;
-And then run cache_fib on two slow inputs:
-(* These will both take a while... *)
-# cache_fib 35
-- : int = 14930352
-# cache_fib 40
-- : int = 165580141
-(* Second time for each value will be instant! *)
-# cache_fib 35
-- : int = 14930352
-# cache_fib 40
-- : int = 165580141
-Additional Hint: you’ll need to play around with the “timing” of when to create a ref cell when
-you define cache. When should the ref be created? The solution is short but a little tricky. *)
-
-(* return a reference dictionary *)
+(* part 1 *)
 
 (* borrowing dict tree from part 9 *)
 type ('k, 'v) dict_tree = 
@@ -75,21 +49,32 @@ let rec insert (tr : ('k, 'v) dict_tree) k v =
       Br (new_height, k', v', l, new_right)
 ;;
 
-let ref_dict_tree = ref Lf;;
-
-let cache f n =
-  let result = lookup !ref_dict_tree n in
-  match result with
-  | Some value -> value
-  | None -> 
-    let f_res = f n in
-    ref_dict_tree := insert !ref_dict_tree n f_res;
-    f_res
+let rec print_tree ?(indent="") = function
+  | Lf -> Printf.printf "%sLf\n" indent
+  | Br (_, key, value, left, right) ->
+      Printf.printf "%sBr (key: %s, value: %s)\n" indent (string_of_int key) 
+        (match value with
+         | Some v -> v
+         | None -> "None");
+      let new_indent = indent ^ "  " in
+      print_tree ~indent:new_indent left;
+      print_tree ~indent:new_indent right
   ;;
+
+let cache f =
+  let ref_dict_tree = ref Lf in
+  fun n ->
+    match lookup !ref_dict_tree n with
+    | Some value -> value
+    | None ->
+      let f_res = f n in
+      ref_dict_tree := insert !ref_dict_tree n f_res;
+      f_res
+    ;;
 
 let rec fib n = if n = 0 || n = 1 then 1 else fib (n-1) + fib (n-2)
 
-let cached_fib = cache fib
+let cached_fib = cache fib;;
 
     
 let () =
@@ -97,5 +82,49 @@ let () =
   Printf.printf "fib 40: %d\n" (cached_fib 40);
   (* Second time for each value will be instant! *)
   Printf.printf "fib 35: %d\n" (cached_fib 35);
-  Printf.printf "fib 40: %d\n" (cached_fib 40)
+  Printf.printf "fib 40: %d\n" (cached_fib 40);
 ;;
+
+
+(* part 2 *)
+
+let array_fold_left f acc arr =
+  let rec aux acc idx = 
+    if idx < Array.length arr then
+      let new_acc = f acc arr.(idx) in
+      aux new_acc (idx + 1)
+    else 
+      acc
+    in
+    aux acc 0
+  ;;
+
+
+let sum_array arr = 
+  let sum = array_fold_left ( + ) 0 arr in
+  Printf.printf "Sum of array elements: %d\n" sum;
+  sum
+;;
+
+let example_array = [|1; 2; 3; 4; 5|];;
+let result = sum_array example_array;;
+
+let array_fold_right f arr acc = 
+  let rec aux acc idx =
+    if idx < 0 then
+      acc
+    else
+      aux (f acc arr.(idx)) (idx-1)
+    in
+    aux acc ((Array.length arr) - 1)
+  ;;
+
+let sum_array_right arr = 
+    let sum = array_fold_right ( + ) arr 0 in
+    Printf.printf "Sum of array elements: %d\n" sum;
+    sum
+  ;;
+  
+  let right_example_array = [|1; 2; 3; 4; 5|];;
+  let right_result = sum_array_right right_example_array;;
+  
